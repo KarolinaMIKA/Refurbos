8715f02b516976870ca81ffa456a51cce26b045e
KarolinaMika <karolina.mikailaite@gmail.com>
2022-05-02 23:37:38 +0300

    not yet

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
-function incrementValue(e) {
-    e.preventDefault();
-    var fieldName = $(e.target).data('field');
-    var parent = $(e.target).closest('div');
-    var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
-  
-    if (!isNaN(currentVal)) {
-      parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
-    } else {
-      parent.find('input[name=' + fieldName + ']').val(0);
-    }
-  }
-  
-  function decrementValue(e) {
-    e.preventDefault();
-    var fieldName = $(e.target).data('field');
-    var parent = $(e.target).closest('div');
-    var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
-  
-    if (!isNaN(currentVal) && currentVal > 0) {
-      parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
-    } else {
-      parent.find('input[name=' + fieldName + ']').val(0);
-    }
-  }
-  
-  $('.input-group').on('click', '.button_plus', function(e) {
-    incrementValue(e);
-  });
-  
-  $('.input-group').on('click', '.button_minus', function(e) {
-    decrementValue(e);
-  });
-  
+$(document).ready(function() {
+    $('.button_minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.button_plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
